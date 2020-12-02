385853bca91950088d122548634087a75f2d419a
Dillon Mulroy <dillon.mulroy@formidable.com>
2020-12-02 09:01:56 -0500

    rename vars

--- 2020/02/part2.js
+++ 2020/02/part2.js
@@ -3,22 +3,22 @@ const path = require('path');
 
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
 const entries = input.split('\n').map((entry) => {
-  const [minMax, untrimmedLetter, password] = entry.split(' ');
-  const [min, max] = minMax.split('-');
+  const [indexes, untrimmedLetter, password] = entry.split(' ');
+  const [firstIndex, secondIndex] = indexes.split('-');
   const letter = untrimmedLetter.replace(':', '');
 
   return {
     letter,
-    firstIdx: Number(min) - 1,
-    secondIdx: Number(max) - 1,
+    firstIndex: Number(firstIndex) - 1,
+    secondIndex: Number(secondIndex) - 1,
     password,
   };
 });
 
 const numValidPasswords = entries.reduce(
-  (count, { letter, firstIdx, secondIdx, password }) => {
-    const firstLetter = password[firstIdx];
-    const secondLetter = password[secondIdx];
+  (count, { letter, firstIndex, secondIndex, password }) => {
+    const firstLetter = password[firstIndex];
+    const secondLetter = password[secondIndex];
 
     if (firstLetter === letter || secondLetter === letter) {
       if (firstLetter === secondLetter) {
